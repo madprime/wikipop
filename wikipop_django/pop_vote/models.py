3f16bd4898971ea56d08efbbbd567bd407f90cfb
Madeleine Price Ball <mad-github@printf.net>
2014-06-22 13:50:49 -0400

    Put in some data

--- wikipop_django/pop_vote/models.py
+++ wikipop_django/pop_vote/models.py
@@ -3,8 +3,8 @@ from django.db import models
 class WPArticle(models.Model):
     """WP article title and popularity."""
     title = models.CharField(max_length=256)
-    median_popularity = models.IntegerField()
+    mdn_pop = models.DecimalField(max_digits=5, decimal_places=1)
 
     def __unicode__(self):
         """Return string containing title and median popularity"""
-        return self.title + ", " + str(self.median_popularity)
+        return self.title + ", " + str(self.mdn_pop)
--- wikipop_django/pop_vote/views.py
+++ wikipop_django/pop_vote/views.py
@@ -1,5 +1,22 @@
 # Create your views here.
+from decimal import *
+import random
 from django.http import HttpResponse
+from django.shortcuts import render
+from .models import WPArticle
+
+# More popular article must be more than (1 + MIN_SEP)-fold more popular,
+# to avoid presenting pairs with nearly-equal popularity.
+MIN_SEP = 0.5
 
 def index(request):
-    return HttpResponse("Wikipop! home page")
+    wp_articles = WPArticle.objects.all()
+    wp_page1 = random.choice(wp_articles)
+    wp_page2 = random.choice(wp_articles)
+    while (wp_page2.mdn_pop * Decimal(1.0 + MIN_SEP) > wp_page1.mdn_pop and
+           wp_page1.mdn_pop * Decimal(1.0 + MIN_SEP) > wp_page2.mdn_pop):
+        wp_page2 = random.choice(wp_articles)
+        
+    return render(request, 'pop_vote/index.html',
+                  {'wp_page1': wp_page1,
+                   'wp_page2': wp_page2})
