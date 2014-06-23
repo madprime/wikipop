19406085e70071c10b3260653a4c3d3496b44e2e
Madeleine Price Ball <mad-github@printf.net>
2014-06-22 23:28:00 -0400

    Record votes

--- wikipop_django/pop_vote/models.py
+++ wikipop_django/pop_vote/models.py
@@ -8,3 +8,10 @@ class WPArticle(models.Model):
     def __unicode__(self):
         """Return string containing title and median popularity"""
         return self.title + ", " + str(self.mdn_pop)
+
+class Vote(models.Model):
+    """Record votes."""
+    article1 = models.ForeignKey(WPArticle, related_name='article1')
+    article2 = models.ForeignKey(WPArticle, related_name='article2')
+    vote = models.ForeignKey(WPArticle, related_name='vote')
+    timestamp = models.DateTimeField(auto_now_add=True)
--- wikipop_django/pop_vote/views.py
+++ wikipop_django/pop_vote/views.py
@@ -6,7 +6,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.views import generic
 
-from .models import WPArticle
+from .models import WPArticle, Vote
 
 # More popular article must be more than (1 + MIN_SEP)-fold more popular,
 # to avoid presenting pairs with nearly-equal popularity.
@@ -30,6 +30,19 @@ class IndexView(generic.TemplateView):
     def post(self, request, *args, **kwargs):
         wp_page1 = WPArticle.objects.get(title=request.POST['wp_page1'])
         wp_page2 = WPArticle.objects.get(title=request.POST['wp_page2'])
+        vote = WPArticle.objects.get(title=request.POST['pop_vote'])
+        vote = Vote(article1=wp_page1,
+                    article2=wp_page2,
+                    vote=vote)
+        vote.save()
+
+        num_votes = Vote.objects.filter(
+            article1__title=request.POST['wp_page1'],
+            article2__title=request.POST['wp_page2']).count()
+        num_vote_same = Vote.objects.filter(
+            article1__title=request.POST['wp_page1'],
+            article2__title=request.POST['wp_page2'],
+            vote__title=request.POST['pop_vote']).count()
 
         is_correct = False
         if ((request.POST['pop_vote'] == request.POST['wp_page1'] and
@@ -41,4 +54,6 @@ class IndexView(generic.TemplateView):
         return render(request, 'pop_vote/result.html',
                       {'wp_page1': wp_page1,
                        'wp_page2': wp_page2,
-                       'is_correct': is_correct})
+                       'is_correct': is_correct,
+                       'num_votes': num_votes,
+                       'num_vote_same': num_vote_same })
